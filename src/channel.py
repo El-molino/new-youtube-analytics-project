862737dd6926b21ef913b34df3ae025587457cd2
el <Nosb1992@gmail.com>
2023-08-01 18:12:03 +0300

    add homework 2

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,4 @@
+import json
 import os
 from googleapiclient.discovery import build
 from dotenv import load_dotenv
@@ -14,7 +15,46 @@ class Channel:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
 
+        service = Channel.get_service()
+        parts = 'snippet,statistics'
+        result = service.channels().list(id=channel_id, part=parts).execute()
+        channel = result['items'][0]['snippet']
+        statistics = result['items'][0]['statistics']
+
+        self.id = channel_id
+        self.title = channel['title']
+        self.description = channel['description']
+        self.url = f"https://www.youtube.com/channel/{self._channel_id}"
+        self.subscriber_count = statistics['subscriberCount']
+        self.video_count = statistics['videoCount']
+        self.view_count = statistics['viewCount']
+
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
         print(channel)
+
+
+@classmethod
+def get_service(cls):
+    """
+    Возвращает объект для работы с YouTube API
+    """
+    return youtube
+
+
+def to_json(self, filename):
+    """
+    Сохраняет в файл значения атрибутов экземпляра Channel
+    """
+    data = {
+        'channel_id': self._channel_id,
+        'title': self.title,
+        'about': self.description,
+        'url': self.url,
+        'subscribers_counter': self.subscriber_count,
+        'videos_total': self.video_count,
+        'views_total': self.view_count
+    }
+    with open(filename, 'w') as f:
+        json.dump(data, f)
